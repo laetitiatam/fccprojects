b5ceea4aa5ccd507ee5ee0bc3a0d51e5fb7e22dc
laetitiatam <laetitia.tam@gmail.com>
2017-01-17 09:58:59 -0500

    Add files via upload

--- Intermediate Front End Development Projects/Random Quote Machine/js/index.js
+++ Intermediate Front End Development Projects/Random Quote Machine/js/index.js
@@ -1,1 +1,20 @@
+var forismaticAPI = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?';
 
+$(document).ready(function() {
+
+  $.getJSON(forismaticAPI, function(a) {
+    $(".quote").html('<p>"' + a.quoteText + '"</p>' + "<p>— " + a.quoteAuthor + "</p>");
+    updateTweet(a.quoteText, a.quoteAuthor);
+  });
+
+  $("#newquote").on("click", function() {
+    $.getJSON(forismaticAPI, function(a) {
+      $(".quote").html('<p>"' + a.quoteText + '"</p>' + "<p>— " + a.quoteAuthor + "</p>");
+      updateTweet(a.quoteText, a.quoteAuthor);
+    });
+  });
+
+  function updateTweet(quote) {
+    $("#twitterSend").attr("href", "https://twitter.com/intent/tweet?text=" + '"' + arguments[0] + '"' + " - " + arguments[1]);
+  }
+});
